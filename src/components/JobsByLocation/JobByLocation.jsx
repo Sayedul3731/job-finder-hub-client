320d75afe90c243bc9798708a2cf860375995227
MD Sayedul Haque <mdsayedulhaque932@gmail.com>
2023-11-05 11:45:52 -0800

    'Jobs By Location' section customization added

---
+++ src/components/JobsByLocation/JobByLocation.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/prop-types */
+
+
+const JobByLocation = ({ jobLocation }) => {
+    const { img, total, location ,type} = jobLocation;
+    return (
+        <div className="card card-compact w-96 bg-base-100 shadow-xl">
+            <figure><img src={img} className="h-[300px]" alt="Shoes" /></figure>
+            <div className="card-body">
+                <h2 className="card-title text-3xl">{location}</h2>
+                <div className="flex justify-between">
+                    <div className="text-lg">{type}</div>
+                    <div className="text-lg">{total} Companies</div>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default JobByLocation;
--- src/components/JobsByLocation/JobsByLocation.jsx
+++ src/components/JobsByLocation/JobsByLocation.jsx
@@ -1,9 +1,26 @@
-
+import { useEffect, useState } from "react";
+import JobByLocation from "./JobByLocation";
 
 const JobsByLocation = () => {
+
+    const [jobsLocation, setJobsLocation] = useState([])
+
+    useEffect( () => {
+        fetch('location.json')
+        .then(res => res.json())
+        .then(data => setJobsLocation(data))
+    },[])
     return (
         <div>
-            <h1>Jobs By Location</h1>
+            <h1 className="text-4xl font-semibold text-center">Jobs By Location</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-10">
+                {
+                    jobsLocation.map(jobLocation => <JobByLocation
+                    key={jobLocation.id}
+                    jobLocation={jobLocation}
+                    ></JobByLocation>)
+                }
+            </div>
         </div>
     );
 };
