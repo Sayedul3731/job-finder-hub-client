d7a1e5f9f416ed3c21b994c81147185c919c959f
MD Sayedul Haque <mdsayedulhaque932@gmail.com>
2023-11-05 20:24:33 -0800

    Password show and hide functionality added

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../Provider/AuthProvider/AuthProvider";
-import { useContext } from "react"
+import { useContext, useState } from "react"
+import { BsFillEyeFill, BsFillEyeSlashFill } from 'react-icons/bs';
 
 
 const Register = () => {
-
+    const [show, setShow] = useState(false)
     const { userCreate } = useContext(AuthContext)
 
     const handleRegister = e => {
@@ -30,7 +31,7 @@ const Register = () => {
                 <div className="text-center lg:text-left">
                     <h1 className="text-5xl text-white font-bold">Register now!</h1>
                 </div>
-                <div className="rounded-sm border-r border-l px-5 mt-10 w-full md:w-1/2">
+                <div className="relative rounded-sm border-r border-l px-5 mt-10 w-full md:w-1/2">
                     <form onSubmit={handleRegister} className="">
                         <div className="form-control">
                             <label className="label">
@@ -54,7 +55,12 @@ const Register = () => {
                             <label className="label">
                                 <span className="label-text text-lg">Password</span>
                             </label>
-                            <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+                            <input type={show ? 'text' : 'password'} name="password" placeholder="password" className="input input-bordered" required />
+                            <div onClick={() => setShow(!show)} className="absolute ml-[406px] mt-[62px] cursor-pointer">
+                                {
+                                    show ? <BsFillEyeSlashFill></BsFillEyeSlashFill> : <BsFillEyeFill></BsFillEyeFill> 
+                                }
+                            </div>
                         </div>
                         <div className="form-control mt-6">
                             <button type="submit" className="btn bg-sky-500 text-white hover:text-black">Register</button>
