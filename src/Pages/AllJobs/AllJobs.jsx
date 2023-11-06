25a90cd8e0999e02cd4fe29602f2565027a5629a
MD Sayedul Haque <mdsayedulhaque932@gmail.com>
2023-11-06 12:14:19 -0800

    When a user search jobs by job title in the all jobs page then show the specific jobs

--- src/Pages/AllJobs/AllJobs.jsx
+++ src/Pages/AllJobs/AllJobs.jsx
@@ -12,10 +12,31 @@ const AllJobs = () => {
                 setJobs(res.data);
             })
     }, [])
+
+    const handleJobsByTitle = (e) => {
+        e.preventDefault()
+        const inputFieldValue = e.target.search.value;
+        console.log(inputFieldValue);
+        const titleBasedJobs = jobs.filter(job => job.title === inputFieldValue)
+        setJobs(titleBasedJobs)
+        e.target.value = ''
+    }
+
     return (
         <div>
             <h1 className="text-4xl font-semibold text-center mt-10 mb-5">All Jobs Here</h1>
-            <p className="text-end text-2xl font-medium">Total Jobs: {jobs.length}</p>
+            <div className="flex justify-between mb-2 px-3 items-center">
+                <div>
+                <form onSubmit={handleJobsByTitle} className="flex justify-center items-center">
+                    <input type="text" name="search" placeholder="Search…" className="border px-2 py-3" />
+                    <button type="submit" className="bg-gray-300 py-[13px] px-3 ">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                        </svg>
+                    </button>
+                </form>
+                </div>
+                <p className="text-end text-2xl font-medium">Total Jobs: {jobs.length}</p>
+            </div>
             <div className="overflow-x-auto">
                 <table className="table">
                     {/* head */}
@@ -32,10 +53,10 @@ const AllJobs = () => {
                     <tbody>
                         {/* row 1 */}
                         {
-                            jobs.map(job => <AllJobsRow 
+                            jobs.map(job => <AllJobsRow
                                 key={job._id}
                                 job={job}
-                                ></AllJobsRow>)
+                            ></AllJobsRow>)
                         }
                     </tbody>
                 </table>
