d70801f92d7ffced2477a7c5b9ae519d14299f3a
MD Sayedul Haque <mdsayedulhaque932@gmail.com>
2023-11-07 17:55:53 -0800

    error page handle and show a gif

--- src/Pages/AppliedJobs/AppliedJobsRow.jsx
+++ src/Pages/AppliedJobs/AppliedJobsRow.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/prop-types */
 
-
 const AppliedJobsRow = ({ appliedJob }) => {
     console.log(appliedJob);
     const { title, salary, applicants, category, description, logo, photo } = appliedJob;
--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 const ErrorPage = () => {
   return (
-    <div>
-      <h1>Oops!!!</h1>
-      <h1>There is no data...</h1>
+    <div className="border h-screen flex justify-center items-center flex-col">
+      <div className="w-full md:w-5/6 lg:w-4/6 h-3/4 md:h-3/4 lg:h-3/5  flex justify-center items-center">
+        <img className="h-full" src="/page404.webp" alt="" />
+      </div>
+     <Link to="/" className="text-5xl font-semibold mt-5">Back To Home</Link>
     </div>
   );
 };
